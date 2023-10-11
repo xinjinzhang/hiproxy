76f9a30ba5ad97b90ff43ae268b4f74bebcc800d
vincent.zhang <zhangjinxin_2001@163.com>
2023-10-11 15:06:19 +0800

    fix image|image-x-ico gzip content-type cannot show

--- src/flows/proxy/doRequest/request.js
+++ src/flows/proxy/doRequest/request.js
@@ -68,7 +68,7 @@ module.exports = {
 
       var contentType = response.headers['content-type'];
       var encoding = response.headers['content-encoding'];
-      var isImageFile = /(image)/.test(contentType);
+      var isImageFile = /(image)(\/.*)?/.test(contentType);
       var needUnZip = (encoding === 'gzip' || encoding === 'deflate') && !isImageFile;
       var isTextFile = /(text|xml|html|plain|json|javascript|css)/.test(contentType);
 
